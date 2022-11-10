4487a61856c5528f5eb1ee34fe5604e11328ad61
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-11-10 19:14:59 +0700

    add method delnode and search

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -65,6 +65,36 @@ namespace singly_linked_list
             nodeBaru.next = current;
             previous.next = nodeBaru;
         }
+        //Method untuk menghapus node tertentu didalam list
+
+        public bool delNode(int nim)
+        {
+            Node previous, current;
+            previous = current = null;
+            //check apakah node yang dimaksud ada di dalam list atau tidak
+            if (Search(nim, ref previous, ref current) == false)
+                return false;
+            previous.next = current.next;
+            if (current == START)
+
+                START = START.next;
+            return true;
+
+        }
+        //Method untuk mengecek apakah node yang dimaksud ada di dalam list atau tidak
+        public bool Search(int nim, ref Node previous, ref Node current)
+        {
+            previous = current;
+            while ((current != null) && (nim != current.noMhs))
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (current == null)
+                return (false);
+            else
+                return (true);
+        }
     }
     class Program
     {
